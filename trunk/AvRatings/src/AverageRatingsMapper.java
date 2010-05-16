4ab8f89c638006cb70b670978bdbcecac19c7ad0
V.Supalov <V.Supalov@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-05-16 23:35:34 +0000

    rest

--- trunk/AvRatings/src/AverageRatingsMapper.java
+++ trunk/AvRatings/src/AverageRatingsMapper.java
@@ -2,16 +2,17 @@ import java.io.IOException;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Mapper;
 
-
-
 public class AverageRatingsMapper extends Mapper<Object, Text, Text, DoubleWritable> {
 	private Text id = new Text();
+	private DoubleWritable rating = new DoubleWritable();
+	
 	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
 		String line = value.toString();
-		id.set(getMovieId(line));
-		double rating = getRating(line);
 		
-		context.write(id, new DoubleWritable(rating));
+		id.set(getMovieId(line));
+		rating.set(getRating(line));
+		
+		context.write(id, rating);
 	}
 
 	private String getMovieId(String line) {
