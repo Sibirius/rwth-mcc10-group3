0f368f9f7007f03c2c1a327ef612284e931c76ee
mailtotheis <mailtotheis@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-06-05 08:17:04 +0000

    thumbnails

--- MMAApp/src/com/rwthmcc103/MMAApp.java
+++ MMAApp/src/com/rwthmcc103/MMAApp.java
@@ -1,13 +1,30 @@
 package com.rwthmcc103;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MMAApp extends Activity {
-    /** Called when the activity is first created. */
-    @Override
+	
+    private Button thumbnailsButton;
+	
+	/** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        this.thumbnailsButton = (Button)this.findViewById(R.id.thumbnails);
+        this.thumbnailsButton.setOnClickListener(new Button.OnClickListener() { 
+        	
+        	public void onClick (View view){ 
+        		Intent myIntent = new Intent(view.getContext(), Thumbnails.class);
+                startActivityForResult(myIntent, 0); 
+        	}
+        });
+        
+        
     }
 }
