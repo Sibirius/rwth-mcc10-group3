98c2594acd46d96f26a2317cd8180711e4dc2714
sterdtmann <sterdtmann@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-06-12 18:58:27 +0000

    some sample data added!

--- trunk/MMAApp/src/com/rwthmcc103/AwsIntegrator.java
+++ trunk/MMAApp/src/com/rwthmcc103/AwsIntegrator.java
@@ -92,6 +92,112 @@ public class AwsIntegrator {
 		return mItemList;
 	}	
 	
+	public List<MediaItem> getSampleImages(){
+		MediaItem m;
+		List<MediaItem> ml= new ArrayList<MediaItem>();
+		
+		m = new MediaItem();
+		m.setId("sample0");
+		m.setIsVideo(false);
+		m.setTitle("Sample 0");
+		m.setDescription("Image Sample Nr 0");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_0.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_0_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);
+
+		m = new MediaItem();
+		m.setId("sample1");
+		m.setIsVideo(false);
+		m.setTitle("Sample 1");
+		m.setDescription("Image Sample Nr 1");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_1.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_1_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);
+		
+		m = new MediaItem();
+		m.setId("sample2");
+		m.setIsVideo(false);
+		m.setTitle("Sample 2");
+		m.setDescription("Image Sample Nr 2");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_2.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_2_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);
+		
+		m = new MediaItem();
+		m.setId("sample3");
+		m.setIsVideo(false);
+		m.setTitle("Sample 3");
+		m.setDescription("Image Sample Nr 3");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_3.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_3_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);
+		
+		m = new MediaItem();
+		m.setId("sample4");
+		m.setIsVideo(false);
+		m.setTitle("Sample 4");
+		m.setDescription("Image Sample Nr 4");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_4.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_4_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);
+		
+		m = new MediaItem();
+		m.setId("sample5");
+		m.setIsVideo(false);
+		m.setTitle("Sample 5");
+		m.setDescription("Image Sample Nr 5");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_5.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_5_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);
+		
+		m = new MediaItem();
+		m.setId("sample6");
+		m.setIsVideo(false);
+		m.setTitle("Sample 6");
+		m.setDescription("Image Sample Nr 6");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_6.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_6_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);	
+		
+		m = new MediaItem();
+		m.setId("sample7");
+		m.setIsVideo(false);
+		m.setTitle("Sample 7");
+		m.setDescription("Image Sample Nr 7");
+		m.setTags("eins zwei drei");
+		m.setFileURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_7.jpg");
+		m.setThumbnailURI("https://7ecee678-7d24-4cae-8edc-a7bba5e391e7-mcc10group3media.s3.amazonaws.com/sample_7_thumb.jpg");
+		m.setLat("50.77772108971944");
+		m.setLon("6.077810525894165");
+		ml.add(m);		
+		
+		return ml;
+		
+
+		
+	}
+	
 	public List<MediaItem> getFilesByLocation(String type, String lon, String lat, String lonrg, String latrg){
 
 		return new ArrayList<MediaItem>();
