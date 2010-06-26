0a129866c2d46f4c8123e479dcaeaa621b722b11
sterdtmann <sterdtmann@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-06-26 20:42:04 +0000

    a menu, a map, a marker

--- trunk/GeoCatch/GeoCatchApp/src/com/rwthmcc3/GeoCatch.java
+++ trunk/GeoCatch/GeoCatchApp/src/com/rwthmcc3/GeoCatch.java
@@ -1,13 +1,38 @@
 package com.rwthmcc3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class GeoCatch extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	/* Creates the menu items */
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.options_menu, menu);
+	    return true;
+	}
+
+	/* Handles item selections */
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.new_game:
+			return true;
+		case R.id.view_map:
+			startActivityForResult(new Intent(this.getApplicationContext(), com.rwthmcc3.Map.class),0);			
+			return true;			
+		case R.id.prefs:
+			return true;						
+		}
+		return false;
+	}
 }
