e7317f8e1674c6631902e6c51d4749d06094adc7
mailtotheis <mailtotheis@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-06-27 10:45:29 +0000

    startscreen können wir auch entfernen

--- GeoCatch/GeoCatchApp/src/com/rwthmcc3/GeoCatch.java
+++ GeoCatch/GeoCatchApp/src/com/rwthmcc3/GeoCatch.java
@@ -1,39 +1,35 @@
 package com.rwthmcc3;
 
+
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 
-public class GeoCatch extends Activity{
-	
+
+public class GeoCatch extends Activity {
+	/** Called when the activity is first created. */
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+		setContentView(R.layout.on_start);
+		LinearLayout layout = (LinearLayout)findViewById(R.id.on_start_layout);
+        layout.setOnClickListener(doLayoutOnClick);
+        
+        //load list
+        MainMenu.setListofGames();
+
 	}
 
-	/* Creates the menu items */
-	public boolean onCreateOptionsMenu(Menu menu) {
-	    MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.options_menu, menu);
-	    return true;
-	}
-
-	/* Handles item selections */
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.new_game:
-			return true;
-		case R.id.view_map:
-			startActivityForResult(new Intent(this.getApplicationContext(), com.rwthmcc3.Map.class),0);			
-			return true;			
-		case R.id.prefs:
-			return true;						
-		}
-		return false;
-	}
+	
+	OnClickListener doLayoutOnClick = new OnClickListener() {		
+		public void onClick(View view) {
+			startActivityForResult(new Intent(GeoCatch.this, com.rwthmcc3.MainMenu.class),0);
+			}
+	};
 
 }
