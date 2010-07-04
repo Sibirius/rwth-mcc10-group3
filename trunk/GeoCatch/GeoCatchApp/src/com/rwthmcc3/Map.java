c03d127b12eda7b53749d815a2e3befdb25c5b9a
sterdtmann <sterdtmann@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-07-04 20:58:08 +0000

    overlays for route, target, powerups

--- trunk/GeoCatch/GeoCatchApp/src/com/rwthmcc3/Map.java
+++ trunk/GeoCatch/GeoCatchApp/src/com/rwthmcc3/Map.java
@@ -3,7 +3,6 @@ package com.rwthmcc3;
 import java.util.List;
 
 import android.graphics.drawable.Drawable;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,16 +20,21 @@ public class Map extends MapActivity{
 	private MapView mapView;
 	private MapController mapController;
 	
+	private String provider = "";
+	
 	private List<Overlay> mapOverlays;
 	private Drawable startMarker;
 	private Drawable marker;
 	private Drawable curPosMarker;
 	private Drawable targetMarker;
-	private Drawable powerupMarker;
+	private Drawable powerUpMarker;
 	private PositionMarkerOverlay itemizedOverlay;
+	private PositionMarkerOverlay targetOverlay;
+	private PositionMarkerOverlay powerUpOverlay;	
 	
 	private GeoPoint prePoint = null;
-
+	private GeoPoint prePrePoint = null;
+	
 	LocationManager lm;	
 	
 	@Override
@@ -47,13 +51,19 @@ public class Map extends MapActivity{
 		marker = this.getResources().getDrawable(R.drawable.point);
 		curPosMarker = this.getResources().getDrawable(R.drawable.point_blue);
 		targetMarker = this.getResources().getDrawable(R.drawable.point_red);
-		powerupMarker = this.getResources().getDrawable(R.drawable.point_yellow);
+		powerUpMarker = this.getResources().getDrawable(R.drawable.point_yellow);
 		
 		lm = (LocationManager) getSystemService(LOCATION_SERVICE);
-		String bestProvider = lm.getBestProvider(new Criteria(), false);
-		lm.getLastKnownLocation(bestProvider);
-		lm.requestLocationUpdates(bestProvider, 0, 0, new GeoUpdateHandler());
 
+		if(lm.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+			provider = LocationManager.GPS_PROVIDER;
+		} else {
+			provider = LocationManager.NETWORK_PROVIDER;			
+		}
+		
+		lm.getLastKnownLocation(provider);
+		lm.requestLocationUpdates(provider, 0, 0, new GeoUpdateHandler());
+		gameLoop();
 	}	
 	
 	@Override
@@ -61,28 +71,64 @@ public class Map extends MapActivity{
 	    return false;
 	}
 	
+	private void gameLoop(){
+		updateTargetPosition(54654321,6654321);
+		updatePowerUpPosition(54123456,6123456);	
+	}
+	
+	private void updateTargetPosition(int lat, int lng){
+		//TODO: Get target position from server
+		GeoPoint point = new GeoPoint(lat, lng);
+		
+		mapOverlays = mapView.getOverlays();
+		if(targetOverlay != null) mapOverlays.remove(targetOverlay);
+		targetOverlay = new PositionMarkerOverlay(null, null, targetMarker);
+		targetOverlay.addOverlay(new OverlayItem(point, "", ""));
+		mapOverlays.add(targetOverlay);		
+	}
+	
+	private void updatePowerUpPosition(int lat, int lng){
+		//TODO: Get powerup position from server
+
+		GeoPoint point = new GeoPoint(lat, lng);
+		
+		mapOverlays = mapView.getOverlays();
+		if(powerUpOverlay != null) mapOverlays.remove(powerUpOverlay);
+		powerUpOverlay = new PositionMarkerOverlay(null, null, powerUpMarker);
+		powerUpOverlay.addOverlay(new OverlayItem(point, "", ""));
+		mapOverlays.add(powerUpOverlay);		
+	}	
+	
 	public class GeoUpdateHandler implements LocationListener {
 
 		public void onLocationChanged(Location location) {
-			int lat = (int) (location.getLatitude() * 1E6);
-			int lng = (int) (location.getLongitude() * 1E6);		
-			
-			GeoPoint point = new GeoPoint(lat, lng);
-			OverlayItem overlayitem = new OverlayItem(point, "", "");
-
-			mapOverlays = mapView.getOverlays();
-			if(prePoint == null ){
-				itemizedOverlay = new PositionMarkerOverlay(prePoint, point, startMarker);
-			} else {
-				itemizedOverlay = new PositionMarkerOverlay(prePoint, point, marker);
+			if(location != null){
+				int lat = (int) (location.getLatitude() * 1E6);
+				int lng = (int) (location.getLongitude() * 1E6);		
+				
+				GeoPoint point = new GeoPoint(lat, lng);
+				//TODO: Save location in player class / send it to server
+				
+				mapOverlays = mapView.getOverlays();
+				if(prePoint == null ){
+					itemizedOverlay = new PositionMarkerOverlay(null, null, startMarker);
+				} else {
+					if(prePrePoint != null){
+						if(itemizedOverlay != null) mapOverlays.remove(itemizedOverlay);
+						PositionMarkerOverlay itemizedOverlay2 = new PositionMarkerOverlay(prePrePoint, prePoint, marker);
+						itemizedOverlay2.addOverlay(new OverlayItem(prePoint, "", ""));
+						mapOverlays.add(itemizedOverlay2);
+					}
+					itemizedOverlay = new PositionMarkerOverlay(prePoint, point, curPosMarker);
+				}
+				itemizedOverlay.addOverlay(new OverlayItem(point, "", ""));
+				mapOverlays.add(itemizedOverlay);
+				
+				mapController.animateTo(point);		
+				
+				prePrePoint = prePoint;
+				prePoint = point;
 			}
-			itemizedOverlay.addOverlay(overlayitem);
-			mapOverlays.add(itemizedOverlay);
-			
-			mapController.animateTo(point);		
-			
-			prePoint = point;
-
 		}
 
 		public void onProviderDisabled(String provider) {
