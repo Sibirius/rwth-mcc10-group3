97ebd25a2c17261502ea17bcd73b175458062843
mailtotheis <mailtotheis@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-07-04 10:11:28 +0000

    onLongClickListItem

--- trunk/GeoCatch/GeoCatchApp/src/com/rwthmcc3/MainMenu.java
+++ trunk/GeoCatch/GeoCatchApp/src/com/rwthmcc3/MainMenu.java
@@ -5,16 +5,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemLongClickListener;
 
 
 
@@ -23,29 +29,56 @@ public class MainMenu extends ListActivity{
 	private static ArrayList<HashMap<String, String>> mylist = new ArrayList<HashMap<String, String>>();
 	private SimpleAdapter mSchedule;
 	private static String LOGTAG = "MainMenu";
+	private AlertDialog alert;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-	
+        
+        //create list
         ListView lv = getListView();
 	    lv.setTextFilterEnabled(true);
-	    
-	    //message to user and load list 
-	    ProgressDialog dialog = ProgressDialog.show(MainMenu.this, "", 
-                "Spielliste wird vom Server abgerufen. Bitte warten...", true);
-	    setListofGames();
-	    dialog.dismiss();
-	    
-	    
+	    lv.setOnItemLongClickListener(doListItemOnClick);
 	    mSchedule = new SimpleAdapter(this, mylist, R.layout.main_menu_list_item,
-	                new String[] {"game_name", "player_count", "distance"}, new int[] {R.id.game_name, R.id.player_count_list, R.id.distance});
+                new String[] {"game_name", "player_count", "distance"}, new int[] {R.id.game_name, R.id.player_count_list, R.id.distance});
 	    lv.setAdapter(mSchedule);
-	    	    
+	    
+	    
+	    //load list 
+	    setListofGames();
+	    
+	    
+	    //create dialog
+	    final CharSequence[] items = {"Spiel beitreten", "Spielerliste anzeigen"};
+	    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	    builder.setTitle("Bitte auswählen:");
+	    builder.setItems(items, new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int item) {
+	            Toast.makeText(getApplicationContext(), items[item], Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	    alert = builder.create();
+	    
     };
 	
     
+    OnItemLongClickListener doListItemOnClick = new OnItemLongClickListener() {
+		@Override
+		public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+				int arg2, long arg3) {
+			alert.show();
+			return false;
+		}		
+    };
     
+    
+    
+    /**
+     * Adds a single item to the game-list.
+     * @param game_name
+     * @param player_count
+     * @param distance
+     */
 	public static void addItemToList(String game_name, String player_count, String distance){
 		HashMap<String, String> map = new HashMap<String, String>();
 	    map.put("game_name", game_name);
@@ -56,25 +89,28 @@ public class MainMenu extends ListActivity{
 	}
 	
 	
-	
+	/**Calls setListofGames() and notifies list adapter.
+	 * 
+	 */
 	public void updateList(){
 		setListofGames();
-		
-		//TODO test delete
-		mylist.remove(2);
-		mylist.remove(2);
-		
-	
 		mSchedule.notifyDataSetChanged();
 	}
 	
-	
-	public static void setListofGames(){
+	/**Clears game-list and adds new games to the list.
+	 * Calls integrator getGameList(). Notifies user.
+	 * 
+	 */
+	public void setListofGames(){
 		
+		//message to user and load list 
+	    ProgressDialog dialog = ProgressDialog.show(MainMenu.this, "", 
+                "Spielliste wird vom Server abgerufen. Bitte warten...", true);
+	    
 		//delete list before set new list
 		mylist.clear();
 		
-		//TODO getGameList()
+		//TODO set distance
 		
 		//for every item: addItemToList
 		//Player player = Integrator.registerPlayer("F1:12:23:34:45:56", "playertest");
@@ -96,19 +132,25 @@ public class MainMenu extends ListActivity{
 		addItemToList("Resident Evil","4/7 Player","Distance to Creator: 4.5 km");
 		*/
 		
+		
+		
+	    dialog.dismiss();
 	}
 	
 	
-	/* Creates the menu items */
+	/**
+	 *  Creates the menu items. 
+	 *  
+	 */
 	public boolean onCreateOptionsMenu(Menu menu) {
 	    MenuInflater inflater = getMenuInflater();
 	    inflater.inflate(R.menu.main_options_menu, menu);
-	    
-       
 	    return true;
 	}
 
-	/* Handles item selections */
+	/** Handles item selections 
+	 *
+	 */
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.main_options_menu_update:
