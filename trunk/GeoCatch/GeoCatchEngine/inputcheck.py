792da8f51dabff35a3e247c6e284b9fc63e1447e
V.Supalov <V.Supalov@7db7be73-28b2-f0f5-4345-f28a20a1ec41>
2010-07-18 21:26:01 +0000

    doc doc

--- trunk/GeoCatch/GeoCatchEngine/inputcheck.py
+++ trunk/GeoCatch/GeoCatchEngine/inputcheck.py
@@ -6,22 +6,27 @@
 import re
 
 def checkKey(key):
-	""" check if key is a google store type key """
-	#TODO: wrong
+	""" check if key is a google store type key
+		raise Value Error otherwise
+	"""
 	if re.match("^[a-zA-Z0-9-_]{8,100}$", key) != None:
 		return key
 	else:
 		raise ValueError()
 
 def checkMac(mac):
-	""" check mac for form xx:xx:xx:xx where x is alphanumeric(?) """
+	""" check mac for form xx:xx:xx:xx where x is a hex literal
+		raise Value Error otherwise
+	"""
 	if re.match("^[a-fA-F0-9]{2}(:[a-fA-F0-9]{2}){5}$", mac) != None:
 		return mac
 	else:
 		raise ValueError()
 
 def checkName(name):
-	""" check if name is valid (only alphanumeric without spaces(?) """
+	""" check if name is valid (alphanumeric literals and "-_ ." are valid)
+		raise Value Error otherwise
+	"""
 	#check if string long enough
 	if re.match("^[a-zA-Z0-9-_ .]{3,20}$", name) != None:
 		return name
@@ -29,14 +34,18 @@ def checkName(name):
 		raise ValueError()
 
 def checkInt(n):
-	""" check if string carries a valid integer (captain state the obvious: "it should be positive") """
+	""" check if string carries a valid integer (Captain State The Obvious: "It should be positive!")
+		raise Type Error otherwise
+	"""
 	if n.isdigit():
 		return int(n)
 	else:
 		raise TypeError() #todo: more precise?
 
 def checkLocation(loc):
-	""" check if "lat,lon"  lat|lon = xx.xxxx """
+	""" check if loc string has the form "lat,lon" where lat|lon = x(x).x(xxx)
+		raise Value Error otherwise
+	"""
 	#TODO: what length can the lat lon coords be? xx.xxxxx?
 	if re.match("^\d{1,2}.\d{1,20},\d{1,2}.\d{1,20}$", loc) != None:
 		return loc
@@ -44,5 +53,6 @@ def checkLocation(loc):
 		raise ValueError()
 		
 if __name__ == "__main__":
+	#simple tests
 	print checkMac("a4:5f:3b:5B:3b:34")
 	print checkLocation("13.23,34.324")
